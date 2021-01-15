593ad11ab36a18195098b4ea3667311e58e41629
Amos <amos@AmosdeiMac-Pro.local>
2021-01-15 20:09:20 +1100

    rebuild blog

---
+++ src/components/navigation.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+    return (
+        <nav className="site-navigation">
+            <span>My blog blog</span>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    )
+}
